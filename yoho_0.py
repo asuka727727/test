954211511272fe4a52d542c7255bc1e4a7d8d12c
asuka727727 <165292409+asuka727727@users.noreply.github.com>
2024-06-01 20:54:52 +0900

    Update yoho_0.py

--- yoho_0.py
+++ yoho_0.py
@@ -222,8 +222,8 @@ def main():
                
         # 撮影開始時の三角形の面積を計算
         initial_triangle_area = calculate_triangle_area(keypoints_list[0], scores_list[0])
-　　　  if initial_triangle_area is None:
-    　　　　　initial_triangle_area = 0 
+　　　     if initial_triangle_area is None:
+    　　      initial_triangle_area = 0 
         
         # 三角形の面積の変化率を計算
         final_triangle_areas = [calculate_triangle_area(keypoints, scores) for keypoints, scores in zip(keypoints_list, scores_list)]
