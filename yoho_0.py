e1c06fc10a439344ded03796bb03d7ceeff88174
asuka727727 <165292409+asuka727727@users.noreply.github.com>
2024-06-05 10:08:50 +0900

    Update yoho_0.py

--- yoho_0.py
+++ yoho_0.py
@@ -178,7 +178,7 @@ def main():
     
     # 撮影ボタンを作成
     if st.sidebar.button('撮影開始'):
-        cap = cv.VideoCapture(0+cv2.CAP_DSHOW)
+        cap = cv2.VideoCapture(0 + cv2.CAP_DSHOW)
         if not cap.isOpened():
            st.error("カメラを開けませんでした。カメラが接続されているか確認してください。")
            return
