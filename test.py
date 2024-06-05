43f0568950ca8f9ebb389bc8fbcbf5957631f3a0
asuka727727 <165292409+asuka727727@users.noreply.github.com>
2024-06-05 11:10:33 +0900

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import cv2
-
-for i in range(10): # デバイスIDを0から9まで試す
-    cap = cv2.VideoCapture(i)
-    if cap.isOpened():
-        print(f"Camera found at index: {i}")
-        cap.release()
-    else:
-        print(f"No camera found at index: {i}")
